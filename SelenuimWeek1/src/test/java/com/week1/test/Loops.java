97bec480cab5a51a5b7c911276c22bbf919abfc5
mohammad <mohammad@DESKTOP-U1OQ5AI.fios-router.home>
2019-10-17 17:51:33 -0400

    This is first commit

---
+++ SelenuimWeek1/src/test/java/com/week1/test/Loops.java
@@ -0,0 +1,25 @@
+package com.week1.test;
+
+
+import java.util.Scanner;
+
+public class Loops {
+
+	
+
+	public static void main(String[] args) {
+		int a=1;int b=10;
+		while(a++<--b); 
+			System.out.println(a);
+		
+	
+
+
+		
+		}
+	
+	
+			
+
+}
+
